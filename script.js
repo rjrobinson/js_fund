9eea62e93bc9fafc878cfe809768ac886fae694f
RJ Robinson <robertj.robinson@gmail.com>
2014-11-17 17:18:09 -0500

    added fuctions to reuse code

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var foo = doSomething(2); // 48
-var bar = doSomething(3); // 56
-
-function doSomething(paramOne) {
+function doSomething(paramOne, paramTwo, fn) {
   paramOne = paramOne + 3;
   paramOne = paramOne + 1;
   paramOne = paramOne * 8;
 
-  return paramOne;
+  return fn(paramOne, paramTwo);
 };
+
+function sum(paramOne, paramTwo) {
+  return paramOne + paramTwo;
+}
+
+function product(paramOne, paramTwo) {
+  return paramOne * paramTwo;
+}
+
+var foo = doSomething(2, 2, sum); // 96
+var bar = doSomething(3, 2, product); // 112
+
 
 alert(foo);
 alert(bar);
