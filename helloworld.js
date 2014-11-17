1760617cf9ab1a12c6a212da901915f6827e91f8
RJ Robinson <robertj.robinson@gmail.com>
2014-11-17 14:16:59 -0500

    finished lesson 2

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,9 @@
-alert('Hello, World!')
+var helloWorld; // undefinded
 
-alert('Hello, JS Fundamentals! ')
+helloWorld = 'Hello, World!'
+
+var ten = 10
+var pi = 3.14
+var isBoolean = true
+
+alert(myVariable);
