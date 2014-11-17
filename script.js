6536fec8d82ec66bc2b04727ba76ef937be1956b
RJ Robinson <robertj.robinson@gmail.com>
2014-11-17 15:09:57 -0500

    added first function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var foo = parseInt('3.14', 10); // 34
+// set one
+var foo = 2 + 3; // 5
+foo = foo + 1; // 6
+foo = foo * 8; // 48
 
-var bar = parseFloat('3.14', 10); // 34
+// set two
+var bar = 3 + 3;
+bar += 1;
+bar = bar * 8;
+
+
+function doSomething() {
+
+}
