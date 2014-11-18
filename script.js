a7f1bb588f0a994554045bfc5cac20852e3d8d24
RJ Robinson <robertj.robinson@gmail.com>
2014-11-18 12:21:47 -0500

    learned about JS prop and methods)

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-var globalVar = 'This is a global variable.';
+var obj = "This is a string object.";
+length = obj.length; // 24
 
-var globalFunction = function(paramOne) {
-  var localVar = 'This is a local variable.';
+index = obj.indexOf('is'); // 2
+index2 = obj.indexOf('is', index + 1); // 5
+index3 = obj.indexOf('this'); // -1
 
-  var localFunction = function() {
-    var localVar = 'hello, world!';
-
-    alert(localVar);
-  };
-
-  localFunction();
-  alert(localVar);
-};
-
-globalFunction(2);
+console.log(obj.toUpperCase());
+console.log(obj.toLowerCase());
