7323b45bf998b01a2630ddfd37a10f849132460b
RJ Robinson <robertj.robinson@gmail.com>
2014-11-18 12:26:50 -0500

    modified script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ index = obj.indexOf('is'); // 2
 index2 = obj.indexOf('is', index + 1); // 5
 index3 = obj.indexOf('this'); // -1
 
-console.log(obj.toUpperCase());
+alert(obj.toUpperCase());
 console.log(obj.toLowerCase());
