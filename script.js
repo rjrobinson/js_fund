f993190a2799ebe1a7fc1a5a9466fcf02e5ee47d
RJ Robinson <robertj.robinson@gmail.com>
2014-11-25 14:53:35 -0500

    Arrays with JS .

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-// var obj = "This is a string object.";
-// length = obj.length; // 24
-//
-// index = obj.indexOf('is'); // 2
-// index2 = obj.indexOf('is', index + 1); // 5
-// index3 = obj.indexOf('this'); // -1
-//
-// alert(obj.toUpperCase());
-// console.log(obj.toLowerCase());
+// var foo = new Array(11, 'hello', true);
 
-// var person = new Object();
-//
-//
-// person.firstName = 'Robert';
-// person.lastName = 'Robinson';
-//
-// person.getFullName = function() {
-//   return this.firstName + ' ' + this.lastName
-// };
-//
+// items in an array are called elements.
 
-var person = {
-  firstName: 'RJ',
-  lastName: 'Robinson',
-  getFullName: function() {
-    return this.firstName + ' ' + this.lastName;
-  }
-};
+// var foo = [11, 'hello', true];
+//
+// var length = foo.length;
+// var value = foo[0];
+// foo[0] = 12;
+// foo[1] = 13;
+// foo[2] = 'nice';
+// foo[foo.length] = 5;
+//
+//
+// foo.push('a new value with push');
+//
+// alert(foo);
 
-
-alert(person.getFullName());
+// var names = ['Jeremy', 'Jeffery'],
+//   names2 = ['Jenifer', 'Jackie'];
+// var people = names.concat(names2);
+// var joined = people.join(", ")
+// var reversed = people.reverse();
+// var sort = people.sort();
+// alert(sort);
---
+++ using_objects.js
@@ -0,0 +1,31 @@
+// var obj = "This is a string object.";
+// length = obj.length; // 24
+//
+// index = obj.indexOf('is'); // 2
+// index2 = obj.indexOf('is', index + 1); // 5
+// index3 = obj.indexOf('this'); // -1
+//
+// alert(obj.toUpperCase());
+// console.log(obj.toLowerCase());
+
+// var person = new Object();
+//
+//
+// person.firstName = 'Robert';
+// person.lastName = 'Robinson';
+//
+// person.getFullName = function() {
+//   return this.firstName + ' ' + this.lastName
+// };
+//
+
+// var person = {
+//   firstName: 'RJ',
+//   lastName: 'Robinson',
+//   getFullName: function() {
+//     return this.firstName + ' ' + this.lastName;
+//   }
+// };
+//
+//
+// alert(person.getFullName());
