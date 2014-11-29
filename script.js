87d04d7a6713e8d46602faa71b295b36a6981b13
RJ Robinson <robertj.robinson@gmail.com>
2014-11-29 14:57:42 -0500

    added logical conditionals events

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-// var foo = new Array(11, 'hello', true);
+var foo = "hello",
+  bar = 5;
+// logicall AND &&
+if (foo = 'hello' || bar < 6) {
+  alert('True');
+};
 
-// items in an array are called elements.
-
-// var foo = [11, 'hello', true];
-//
-// var length = foo.length;
-// var value = foo[0];
-// foo[0] = 12;
-// foo[1] = 13;
-// foo[2] = 'nice';
-// foo[foo.length] = 5;
-//
-//
-// foo.push('a new value with push');
-//
-// alert(foo);
-
-// var names = ['Jeremy', 'Jeffery'],
-//   names2 = ['Jenifer', 'Jackie'];
-// var people = names.concat(names2);
-// var joined = people.join(", ")
-// var reversed = people.reverse();
-// var sort = people.sort();
-// alert(sort);
+alert('This is outside the statement')
