5af7ef229c81ea77a145d2ace33ea831aadd15d7
RJ Robinson <robertj.robinson@gmail.com>
2014-12-01 21:33:53 -0500

    functions in the window

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-var names = ['Jeremy', 'rj', 'lex']
+(function() {
 
-for (var i = 0, len = names.length; i < len; i = i + 1) {
-  var name = names[i]
+  var foo = 'hello, window';
 
-  alert(name);
-}
+  var bar = function() {
+    var foo = 'Hello, Function';
+
+    alert(foo)
+  };
+
+  bar();
+
+}());
