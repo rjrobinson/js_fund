7ed033a9d9196b655e005e5423f53c60a8ad1b85
RJ Robinson <robertj.robinson@gmail.com>
2014-12-04 15:37:18 -0500

    modifying classes with js.

--- script.js
+++ script.js
@@ -3,10 +3,9 @@
   var divFoo = document.getElementById("foo"),
     style = divFoo.style
 
-  style.color = 'blue';
-  style.border = "1px solid black";
-  style.backgroundColor = '#ffff00';
-  style.padding = "2px";
+  divFoo.className = 'css-class css-class2';
+  divFoo.className = '';
 
+  // alert(style.color);
 
 }());
