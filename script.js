4a58d6610f4e0672ca7993fe5afdd68dc5759b0f
RJ Robinson <robertj.robinson@gmail.com>
2014-12-05 08:56:41 -0500

    finished the Standard Event Model

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 
   var buttons = document.getElementsByTagName("button");
 
-  for (var i = 0, len = buttons.length; i < len; i++) {
-    buttons[i].onclick = function() {
-      var className = this.innerHTML.toLowerCase();
+  var buttonClick = function() {
+    var className = this.innerHTML.toLowerCase();
 
-      document.body.className = className;
-    };
-  }
+    document.body.className = className;
+  };
+
+  for (var i = 0, len = buttons.length; i < len; i++) {
+    // buttons[i].onclick = function() {
+    //   var className = this.innerHTML.toLowerCase();
+    //
+    //   document.body.className = className;
+    // };
+
+
+    buttons[i].addEventListener('click', buttonClick, false)
+    buttons[i].addEventListener('click', function() {
+      alert('hi');
+    }, false)
+
+    buttons[i].removeEventListener('click', buttonClick, false)
+    buttons[i].removeEventListener('click', function() {
+      alert('hi');
+    });
+  };
 
 }()); // main function
