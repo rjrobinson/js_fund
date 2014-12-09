913c8bd3e1fb97500a957b9dd5a1ec2902629faf
RJ Robinson <robertj.robinson@gmail.com>
2014-12-08 19:23:56 -0500

    comments

--- script_fundamentals.js
+++ script_fundamentals.js
@@ -23,21 +23,21 @@
 
 
 
-  // var buttons = document.getElementsByTagName('a');
-  //
-  // var buttonClick = function(evt) {
-  //   var target = eventUtility.getTarget(evt),
-  //     className = target.innerHTML.toLowerCase();
-  //
-  //   eventUtility.preventDefault(evt);
-  //
-  //   document.body.className = className;
-  // };
-  //
-  // for (var i = 0, len = buttons.length; i < len; i++) {
-  //   eventUtility.addEvent(buttons[i], "click", buttonClick);
-  //   //removeEvent(buttons[i], "click", buttonClick);
-  //
-  // }
+  var buttons = document.getElementsByTagName('a');
+
+  var buttonClick = function(evt) {
+    var target = eventUtility.getTarget(evt),
+      className = target.innerHTML.toLowerCase();
+
+    eventUtility.preventDefault(evt);
+
+    document.body.className = className;
+  };
+
+  for (var i = 0, len = buttons.length; i < len; i++) {
+    eventUtility.addEvent(buttons[i], "click", buttonClick);
+    //removeEvent(buttons[i], "click", buttonClick);
+
+  }
 
 }());
